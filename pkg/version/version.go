b46948e0f281490c46d7a8c2c81f73b663610f24
cgil <lao.tseu.is.alive@gmail.com>
2024-09-18 11:33:35 +0200

    with groups in jwt token

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goCloudK8sUserGroup"
 	AppSnake   = "go-cloud-k8s-user-group"
-	VERSION    = "0.3.3"
+	VERSION    = "0.3.4"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/go-cloud-k8s-user-group"
 	REVISION   = "unknown"
 	Build      = "unknown"
